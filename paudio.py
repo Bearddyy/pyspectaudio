31de87c9839829cb67ddd41bca58bba34b3c5aaf
Dan <byrne_daniel@hotmail.co.uk>
2022-01-04 13:52:44 +0000

    better presentation, trying log scale again now

--- paudio.py
+++ paudio.py
@@ -5,6 +5,7 @@ from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
 from kivy.graphics import Color, RoundedRectangle
 from kivy.clock import Clock
+from kivy.core.window import Window
 import random
 import pyaudio
 import threading
@@ -14,15 +15,14 @@ import numpy as np
 class Spectrogram(BoxLayout):
     def __init__(self, **kwargs):
         super(Spectrogram, self).__init__(**kwargs)
-        self.windowWidth = 750
-        self.windowHeight = 500
-        self.centerline = self.windowHeight / 2
-        self.pad = 2
-        self.cols = 129
-        self.rows = 200
-        self.specWidth = (self.windowWidth/self.cols) 
         self.terminateAudioThread = False
-        self.dfft = np.zeros(self.cols)
+        self.dfft = np.zeros(127)
+        self.updateSize()
+
+        col = (49,107,131, 256)
+        col = [i/255 for i in col]
+        #Color(rgba=col)
+        Window.clearcolor = col
         
         event = Clock.schedule_interval(self.drawSpect, 1 / 30.)
 
@@ -34,9 +34,12 @@ class Spectrogram(BoxLayout):
         """
         FORMAT = pyaudio.paInt16 # We use 16bit format per sample
         CHANNELS = 1
-        RATE = 44100
-        CHUNK = 256 # 1024bytes of data red from a buffer
-        RECORD_SECONDS = 2
+        RATE = int(44100)
+        CHUNK = 2048 # 1024bytes of data red from a buffer
+
+        OFFSET = 3
+        LEN = 500 # 4
+        
         p = pyaudio.PyAudio()
         np.seterr(divide = 'ignore')
         stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
@@ -46,23 +49,39 @@ class Spectrogram(BoxLayout):
             # Fast Fourier Transform, 10*log10(abs) is to scale it to dB
             # and make sure it's not imaginary
             try:
-                self.dfft = 10*np.log10(abs(np.fft.rfft(audio_data)))
+                dfft = abs(np.fft.rfft(audio_data))#10*np.log10(abs(np.fft.rfft(audio_data)))
+                #dfft = dfft[2:len(dfft)//LEN]
+                #dfft = dfft.reshape(-1, LEN).mean(axis=1)
+                dfft = dfft[OFFSET:OFFSET+LEN]
+                try:
+                    self.dfft = [((self.dfft[i]*0.8) + (dfft[i] * 0.2) )for i in range(len(dfft))]
+                except:
+                    self.dfft = dfft
             except:
                 pass
-            time.sleep(1/30)
             print(len(self.dfft))
 
-
+    def updateSize(self):
+        size = Window.size
+        self.windowWidth = size[0]
+        self.windowHeight = size[1]
+        self.centerline = self.windowHeight / 2
+        self.pad = 1
+        self.rows = 200
+        self.specWidth = (self.windowWidth/127) 
     
     def drawSpect(self, dt=None):
+        self.updateSize()
         layout = BoxLayout(orientation='vertical')  
         with self.canvas:
             self.canvas.clear()
             for i in range(len(self.dfft)):
                 height = self.dfft[i] #random.randint(10, self.rows) 
+                self.specWidth = (self.windowWidth/len(self.dfft))
                 try:
-                    halfHeight = int(height / 2)
-                    rect = RoundedRectangle(size=(self.specWidth - self.pad, height), pos=(i*self.specWidth, int(self.centerline- (halfHeight)) ))
+                    scaledHeight = (height / 10 )
+                    halfHeight = int(scaledHeight / 2)
+                    RoundedRectangle(size=(self.specWidth - self.pad, scaledHeight), pos=(i*self.specWidth, int(self.centerline- (halfHeight)) ) )
                 except:
                     pass
 
